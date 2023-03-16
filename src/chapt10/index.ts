8fc45cdaeabb7e1a25a39b5b95ebf2d16b2fdadf
Hamberfim <adhamlingithub@gmail.com>
2023-03-16 13:18:41 -0500

    eslint issues fixed

--- src/chapt10/index.ts
+++ src/chapt10/index.ts
@@ -1,8 +1,8 @@
 // console.log("Chapter 10 - Working with object");  // are we watching the right folder?
 // Chapter 10 - Working with object
-let hat = { nameL: "Hat", price: 100 };
+let hat = { name: "Hat", price: 100 };
 let gloves = { name: "Gloves", price: 75 };
 let umbrella = { name: "Umbrella", price: 42 };
 let products = [hat, gloves, umbrella];
 
-products.push(umbrella);
+products.forEach((prod) => console.log(`${prod.name}: ${prod.price}`));
