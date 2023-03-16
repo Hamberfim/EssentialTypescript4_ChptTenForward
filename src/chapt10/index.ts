02bf407e26f59a6f959b72fb6e2bb856c0ac038b
Hamberfim <adhamlingithub@gmail.com>
2023-03-16 12:57:55 -0500

    eslint issues

--- src/chapt10/index.ts
+++ src/chapt10/index.ts
@@ -1,1 +1,8 @@
-console.log("We are in chapt 10");
+// console.log("Chapter 10 - Working with object");  // are we watching the right folder?
+// Chapter 10 - Working with object
+let hat = { nameL: "Hat", price: 100 };
+let gloves = { name: "Gloves", price: 75 };
+let umbrella = { name: "Umbrella", price: 42 };
+let products = [hat, gloves, umbrella];
+
+products.push(umbrella);
