c5e550f454a4bc864f862fa65b8fdd448f8b5c69
Hamberfim <adhamlingithub@gmail.com>
2023-03-17 10:42:24 -0500

    irregular object shape option

--- src/chapt10/index.ts
+++ src/chapt10/index.ts
@@ -1,8 +1,12 @@
 // console.log("Chapter 10 - Working with object");  // are we watching the right folder?
-// Chapter 10 - Working with object
+// Chapter 10 - Working with object  - typescript focus on an objects shape
 let hat = { name: "Hat", price: 100 };
 let gloves = { name: "Gloves", price: 75 };
 let umbrella = { name: "Umbrella", price: 42 };
-let products = [hat, gloves, umbrella];
+// the typescript compiler will still match an object to the defined shape if it has additional properties
+let boots = { name: "Boots", price: 100, waterproof: true };
+
+// object shape type annotation
+let products: { name: string; price: number }[] = [hat, gloves, umbrella, boots];
 
 products.forEach((prod) => console.log(`${prod.name}: ${prod.price}`));
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-console.log(`package.json needs 'start' configured for the director/folder your targeting under dist \n"start": "tsc-watch --onsuccess \\"node dist/YOUR-DIR-HERE/index.js\\"",`);
