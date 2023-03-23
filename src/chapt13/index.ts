82f67d296941a1c48dc65a5ad8fb1cb03d1f2d6b
Hamberfim <adhamlingithub@gmail.com>
2023-03-23 11:30:44 -0500

    start chpt 13

--- src/chapt13/index.ts
+++ src/chapt13/index.ts
@@ -1,26 +1,47 @@
 import { City, Person, Product, Employee } from "./dataTypes";
 
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
+let products = [
+  new Product("Running Shoes", 100),
+  new Product("Hat", 100),
+  new Product("Boots", 100),
+  new Product("Gloves", 25),
+  new Product("Umbrella", 75),
+  new Product("Heavy Coat", 155),
+];
 
 type shapeType = { name: string };
 
 class Collection<T extends shapeType> {
-  constructor(private items: T[] = []) {}
+  private items: Map<string, T>;
+
+  constructor(initialItems: T[] = []) {
+    this.items = new Map<string, T>();
+    this.add(...initialItems);
+  }
 
   add(...newItems: T[]): void {
-    this.items.push(...newItems);
+    newItems.forEach((newItem) => this.items.set(newItem.name, newItem));
   }
 
   get(name: string): T {
-    return this.items.find((item) => item.name === name);
+    return this.items.get(name);
   }
 
   get count(): number {
-    return this.items.length;
+    return this.items.size;
+  }
+
+  values(): Iterator<T> {
+    return this.items.values();
   }
 }
 
 let productCollection: Collection<Product> = new Collection(products);
 console.log(`There are ${productCollection.count} products`);
-let p = productCollection.get("Hat");
-console.log(`Product: ${p.name}, ${p.price}`);
+
+let iterator: Iterator<Product> = productCollection.values();
+let result: IteratorResult<Product> = iterator.next();
+while (!result.done) {
+  console.log(`Product: ${result.value.name}, ${result.value.price}`);
+  result = iterator.next(); // without this call infinity loop
+}
